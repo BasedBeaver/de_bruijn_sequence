93fa66660445df513e007b308129dda319b1b3b3
Daniel <daniel.dimitric91@gmail.com>
2018-12-03 10:38:12 +0100

    debugg F2 DB, needs cycle check

--- LFSR_n4.py
+++ LFSR_n4.py
@@ -1,5 +1,4 @@
 def xor_func(a, b):
-    x = 0
     if not a != (not b):
         x = 1
     else:
@@ -7,7 +6,6 @@ def xor_func(a, b):
     return x
 
 def and_func(a, b):
-    x = 0
     if a and b:
         x = 1
     else:
@@ -31,17 +29,16 @@ print("########################")
 
 
 
-res = [1, 0, 1, 0]
-res_NL = res
+res = [1, 0, 1, 1]
+res_NL = [1, 0, 1, 1]
 while len(res) < 18:
     s1, s2 = res[-4], res[-3]
-    print(s1, s2)
     f = f_func(s1, s2)
     res.append(f)
-    res_NL.append(f)
-    c1, c2, c3 = res[-3], res[-2], res[-1]
-    if c1 and c2 and c3 == 0:
+    c1, c2, c3 = res[-4], res[-3], res[-2]
+    if c1 == 0 and c2 == 0 and c3 == 0:
         res_NL.append(0)
+    res_NL.append(f)
     print("-----------")
 print(res)
 print(res_NL)
