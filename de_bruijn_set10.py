f1f5394e06783e2f5cc79926ee2f1199e8662008
Daniel <daniel.dimitric91@gmail.com>
2018-12-03 13:38:44 +0100

    maybe done?

--- de_bruijn_set10.py
+++ de_bruijn_set10.py
@@ -37,55 +37,63 @@ def f5_func(s1, s2, s3):
 F2 code
 """
 
-res = [0, 1, 0, 0]
-res_NL = [0, 1, 0, 0]
-while len(res) < 16:
-    s1, s2 = res[-4], res[-3]
+res_f2 = [1, 1, 1, 1]
+res_NL_f2 = [1, 1, 1, 1]
+while len(res_f2) < 10003:
+    s1, s2 = res_f2[-4], res_f2[-3]
     f = f2_func(s1, s2)
-    res.append(f)
-    c1, c2, c3 = res[-4], res[-3], res[-2]
+    res_f2.append(f)
+    c1, c2, c3 = res_f2[-4], res_f2[-3], res_f2[-2]
     if c1 == 0 and c2 == 0 and c3 == 0:
-        res_NL.append(0)
-    res_NL.append(f)
-    print("-----------")
-if (res[1] == 0 and res[2] == 0 and res[-1] == 0) or\
-        (res[1] == 0 and res[-2] == 0 and res[-1] == 0):
-    res_NL.append(0)
-print(res)
-print(res_NL)
-print(len(res), len(res_NL))
+        res_NL_f2.append(0)
+    res_NL_f2.append(f)
+if (res_f2[0] == 0 and res_f2[1] == 0 and res_f2[-1] == 0) or\
+        (res_f2[0] == 0 and res_f2[-2] == 0 and res_f2[-1] == 0):
+    res_NL_f2.append(0)
+print(res_f2)
+print(res_NL_f2)
+print(len(res_f2), len(res_NL_f2))
 
 
 """
 F5 code
 """
 
-res = [2, 3, 4, 1]
-res_NL = [2, 3, 4, 1]
+res_f5 = [2, 3, 4, 1]
+res_NL_f5 = [2, 3, 4, 1]
 added = False
-while len(res) < 625:
-    s1, s2, s3 = res[-4], res[-2], res[-1]
+while len(res_f5) < 10003:
+    s1, s2, s3 = res_f5[-4], res_f5[-2], res_f5[-1]
     f = f5_func(s1, s2, s3)
-    res.append(f)
-    c1, c2, c3 = res[-4], res[-3], res[-2]
+    res_f5.append(f)
+    c1, c2, c3 = res_f5[-4], res_f5[-3], res_f5[-2]
     if c1 == 0 and c2 == 0 and c3 == 0 and not added:
-        res_NL.append(0)
+        res_NL_f5.append(0)
         added = True
-    res_NL.append(f)
-    print("-----------")
-if (res[1] == 0 and res[2] == 0 and res[-1] == 0) or\
-        (res[1] == 0 and res[-2] == 0 and res[-1] == 0):
-    res_NL.append(0)
-print(res)
-print(res_NL)
-print(len(res), len(res_NL))
+    res_NL_f5.append(f)
+if (res_f5[0] == 0 and res_f5[1] == 0 and res_f5[-1] == 0) or\
+        (res_f5[0] == 0 and res_f5[-2] == 0 and res_f5[-1] == 0):
+    res_NL_f5.append(0)
+print(res_f5)
+print(res_NL_f5)
+print(len(res_f5), len(res_NL_f5))
 
 
 """
 De Bruijn set 10
 """
+res_NL = []
 
-# out_string = str(res)
-# out_string = ''.join(c for c in out_string if c not in '[],')
-# with open("F5_out.txt", 'w') as file:
-#     file.write(out_string)
+for i in range(0, len(res_NL_f5)):
+    if res_NL_f2[i] == 1:
+        res_NL.append(res_NL_f5[i])
+    elif res_NL_f2[i] == 0:
+        res_NL.append((9 - res_NL_f5[i]))
+print(res_NL)
+print(len(res_NL))
+
+
+out_string = str(res_NL)
+out_string = ''.join(c for c in out_string if c not in '[],')
+with open("db_out.txt", 'w') as file:
+    file.write(out_string)
